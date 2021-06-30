f8679a53d53f643f26295de504a3a531570bb13f
RayssaGermano <82416491+RayssaGermano@users.noreply.github.com>
2021-06-30 17:34:17 -0300

    Add files via upload

--- classes/Pessoa.java
+++ classes/Pessoa.java
@@ -3,24 +3,26 @@ package classes;
 public class Pessoa {
     private String nome;
     private String cpf;
+    private int idade;
 
-    public String getNome(){
+    public String getNome() {
         return nome;
     }
-
-    public void setNome(String nome){
+    public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public String getCpf(){
+    public String getCpf() {
         return cpf;
     }
-
-    public void setCpf(String cpf){
+    public void setCpf(String cpf) {
         this.cpf = cpf;
     }
 
-    public Pessoa() {
+    public int getIdade() {
+        return idade;
     }
-    
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }    
 }
