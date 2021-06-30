e15761694535cc5923d0d2fa1f9c598d638f5e77
RayssaGermano <82416491+RayssaGermano@users.noreply.github.com>
2021-06-30 17:33:41 -0300

    Add files via upload

--- apps/AppPilotos.java
+++ apps/AppPilotos.java
@@ -1,22 +1,18 @@
 package apps;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-
 import classes.Pessoa;
 
 public class AppPilotos {
     public static void main(String[] args) throws InterruptedException, IOException {
-        int MAX_ELEMENTOS = 20;
+        int MAX_ELEMENTOS = 2;
         int opcao, qtdCadastrados = 0;
         Pessoa[] pilotos = new Pessoa[MAX_ELEMENTOS];
-        List<Pessoa> listaPilotos = new ArrayList<Pessoa>();
         Scanner in = new Scanner(System.in);
 
         do {
-            System.out.println("\n****\nMENU\n****\n");
+            System.out.println("\n****MENU****\n");
             System.out.println("1 - Cadastrar piloto");
             System.out.println("2 - Listar pilotos cadastrados");
             System.out.println("3 - Localizar piloto pelo CPF");
@@ -25,85 +21,105 @@ public class AppPilotos {
             System.out.print("Opção: ");
 
             opcao = in.nextInt();
-            in.nextLine(); // Tira o ENTER que ficou na entrada na instrução anterior
+            in.nextLine();
 
             if (opcao == 1) {
-               
                 if (qtdCadastrados == MAX_ELEMENTOS) {
                     System.out.println("\nNão há espaço para cadastrar novos pilotos.");
+                    System.out.println("\nAcesse a opção 4 para alterar o espaço.");
                     voltarMenu(in);
                     continue;
                 }
 
-                
-                Pessoa piloto = new Pessoa();
+                System.out.println("========CADASTRO========");
 
-                System.out.println("Digite um nome");
-                piloto.setNome(in.nextLine());
-                System.out.println("Digite um cpf");
-                piloto.setCpf(in.nextLine());
-                listaPilotos.add(piloto);
-                qtdCadastrados++;
+                try {
+                    Pessoa piloto = new Pessoa();
 
-                System.out.println("\nPiloto cadastrado com sucesso!");
-                voltarMenu(in);
+                    System.out.print("Insira o nome do piloto: ");
+                    piloto.setNome(in.nextLine()); 
+                    System.out.print("Insira o CPF: ");
+                    piloto.setCpf(in.nextLine());
+                    System.out.print("Insira a idade: ");
+                    piloto.setIdade(in.nextInt());
+                    in.nextLine();
+
+                    pilotos[qtdCadastrados] = piloto; 
+                    qtdCadastrados++; 
+
+                    System.out.println("\nPiloto cadastrado com sucesso.");
+                    voltarMenu(in);
+                } catch (Exception e) {
+                    System.out.println("\nOps! Houve um problema ao cadastrar o piloto, tente novamente");
+                    in.nextLine(); 
+                }
             } else if (opcao == 2) {
+                System.out.println("\n==============RELATÓRIO=================\n");
+
                 if (qtdCadastrados == 0) {
-                    System.out.println("\nNão há pilotos cadastrados para exibir.");
+                    System.out.println("\nNão há motoristas cadastrados para exibir.");
                     voltarMenu(in);
                     continue;
-                } else {
-                    System.out.println("\n****\nLista\n****\n");
-                    for (int i = 0; i < listaPilotos.size(); i++) {
-                        System.out.println(
-                                "Nome: " + listaPilotos.get(i).getNome() + "\nCPF: " + listaPilotos.get(i).getCpf());
-                    }
-                    voltarMenu(in);
                 }
 
-                
+                for (Pessoa piloto : pilotos) {
+                    if (piloto != null) {
+                        System.out.printf("\nNome: %s CPF: %s Idade: %s ", piloto.getNome(),
+                                piloto.getCpf(), piloto.getIdade());
+                    }
+                }
                 voltarMenu(in);
             } else if (opcao == 3) {
-                System.out.println("Informe o cpf para localização");
-                String cpfDigitado = in.nextLine();
-                for (Pessoa piloto : listaPilotos) {
-                    if (piloto.getCpf().toString().equals(cpfDigitado)) {
-                        System.out.printf("Piloto:  %s %s", piloto.getNome(), piloto.getCpf());
+                System.out.print("\n========PESQUISA========");
+                System.out.print("\nDigite o CPF para busca: ");
+                String CpfDigitado = in.nextLine(); 
+
+                for (int i = 0; i < pilotos.length + 1; i++) {
+                    try {
+                        if (pilotos[i].getCpf().toString().equals(CpfDigitado)) {
+                            System.out.printf("\nNº: %s Nome: %s CPF: %s Idade: %s ", i + 1,
+                                    pilotos[i].getNome(), 
+                                    pilotos[i].getCpf(), 
+                                    pilotos[i].getIdade());
+                            break;
+                        } else if (i < pilotos.length + 1) {
+                            continue;
+                        }
+                    } catch (Exception e) {
+                        System.out.println("\nPiloto não cadastrado, verifique o CPF e tente novamente.");
                         voltarMenu(in);
                     }
-
                 }
-                ;
-
-                System.out.println("Cpf não encontrado!");
-                voltarMenu(in);
-
             } else if (opcao == 4) {
-                System.out.println("Informe a quantidade desejada");
-                String qtdDigitados;
+                System.out.print("\nInsira a quantidade de registros a ser incrementada: ");
+                int novaQtd = 0;
                 try {
-                    qtdDigitados = in.nextLine();
-                } catch (Exception ex) {
-                    System.out.println("Por favor, digitar apenas números!");
+                    novaQtd = in.nextInt();
+                    if (novaQtd < pilotos.length) { 
+                        System.out.printf("Favor informar um numero maior que %s \n", pilotos.length);
+                        in.nextLine();
+                        voltarMenu(in);
+                    } else { 
+                        Pessoa novaListaPilotos[] = new Pessoa[pilotos.length + novaQtd];
+                        for (int i = 0; i < pilotos.length; i++) {
+                            novaListaPilotos[i] = pilotos[i];
+                        }
+                        MAX_ELEMENTOS = novaListaPilotos.length; 
+                        pilotos = novaListaPilotos;
+                        System.out.println("\nQuantidade incrementada com sucesso!");
+                        voltarMenu(in);
+                    }
+                } catch (Exception e) {
+                    System.out.println("Verifique a quantidade digitada e tente novamente.");
                     in.nextLine();
-                    continue;
-                }
-
-                if (Integer.parseInt(qtdDigitados) < MAX_ELEMENTOS) {
-                    System.out.println("Quantidade não pode ser menor que a cadastrada.");
-                    voltarMenu(in);
-                } else {
-                    MAX_ELEMENTOS = Integer.parseInt(qtdDigitados);
-                    System.out.println("Quantidade atualiza com sucesso!");
                     voltarMenu(in);
                 }
-
+            } else if (opcao != 0) {
                 System.out.println("\nOpção inválida!");
             }
         } while (opcao != 0);
 
         System.out.println("Fim do programa!");
-
         in.close();
     }
 
@@ -111,7 +127,6 @@ public class AppPilotos {
         System.out.println("\nPressione ENTER para voltar ao menu.");
         in.nextLine();
 
-        
         if (System.getProperty("os.name").contains("Windows"))
             new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
         else
@@ -120,4 +135,3 @@ public class AppPilotos {
         System.out.flush();
     }
 }
-
